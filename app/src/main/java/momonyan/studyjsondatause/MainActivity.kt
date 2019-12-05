66e9fd3fa148bde179d370e0e15603f5c53cbea2
“BlueWind_Mac” <zero18030@gmail.com>
2019-12-05 14:58:19 +0900

    [update]読み込みまで

--- app/src/main/java/momonyan/studyjsondatause/MainActivity.kt
+++ app/src/main/java/momonyan/studyjsondatause/MainActivity.kt
@@ -1,12 +1,35 @@
 package momonyan.studyjsondatause
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import org.json.JSONArray
+import org.json.JSONException
+import java.io.BufferedReader
+import java.io.InputStreamReader
+
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val assetManager = resources.assets
+
+        val inputStream = assetManager.open("SampleJsonData.json")
+        val bufferedReader = BufferedReader(InputStreamReader(inputStream))
+        val str: String = bufferedReader.readLine()
+        Log.d("check", str)
+        try {
+            val jsonArray = JSONArray(str)
+            for (i in 0 until jsonArray.length()) {
+                val textView = TextView(this)
+                Log.d("Check", jsonArray[i].toString())
+            }
+        } catch (e: JSONException) {
+            e.printStackTrace()
+        }
     }
 }
