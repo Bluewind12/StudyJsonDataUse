e460ec311c689f5d54363c3a54e928eb6fedc3d9
“BlueWind_Mac” <zero18030@gmail.com>
2019-12-06 17:06:22 +0900

    [update end]表示完成

--- app/src/main/java/momonyan/studyjsondatause/MainActivity.kt
+++ app/src/main/java/momonyan/studyjsondatause/MainActivity.kt
@@ -1,17 +1,20 @@
 package momonyan.studyjsondatause
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import android.util.Log
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
-import org.json.JSONArray
+import kotlinx.android.synthetic.main.activity_main.*
 import org.json.JSONException
+import org.json.JSONObject
 import java.io.BufferedReader
 import java.io.InputStreamReader
 
 
 class MainActivity : AppCompatActivity() {
 
+    @SuppressLint("SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -20,13 +23,33 @@ class MainActivity : AppCompatActivity() {
 
         val inputStream = assetManager.open("SampleJsonData.json")
         val bufferedReader = BufferedReader(InputStreamReader(inputStream))
-        val str: String = bufferedReader.readLine()
+        val str: String = bufferedReader.readText()
         Log.d("check", str)
         try {
-            val jsonArray = JSONArray(str)
+            val jsonObject = JSONObject(str)
+            val jsonArray = jsonObject.getJSONArray("sample")
+            val layout = mainLinerLayout
             for (i in 0 until jsonArray.length()) {
-                val textView = TextView(this)
-                Log.d("Check", jsonArray[i].toString())
+                val jsonData = jsonArray.getJSONObject(i)
+                val nameTextView = TextView(this)
+                val ageTextView = TextView(this)
+                val genderTextView = TextView(this)
+                val otherTextView = TextView(this)
+
+                nameTextView.text = "Name : " + jsonData.getString("name")
+                ageTextView.text = "Age : " + jsonData.getInt("age").toString()
+                genderTextView.text = "Gender : " + if (!jsonData.getBoolean("gender")) {
+                    "男"
+                } else {
+                    "女"
+                }
+                otherTextView.text = "Other : " + jsonData.getString("other")
+                layout.addView(nameTextView)
+                layout.addView(ageTextView)
+                layout.addView(genderTextView)
+                layout.addView(otherTextView)
+                layout.addView(TextView(this))
+
             }
         } catch (e: JSONException) {
             e.printStackTrace()
